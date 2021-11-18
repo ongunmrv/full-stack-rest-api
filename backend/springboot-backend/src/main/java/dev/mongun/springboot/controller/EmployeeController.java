cf60ba2179c7327c151c16784fbcff1dbcb13d9e
merveongun <mongun.dev@gmail.com>
2021-11-18 16:18:46 +0100

    Delete mapping added to backend

--- backend/springboot-backend/src/main/java/dev/mongun/springboot/controller/EmployeeController.java
+++ backend/springboot-backend/src/main/java/dev/mongun/springboot/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package dev.mongun.springboot.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,20 @@ public class EmployeeController {
 	public List<Employee> getEmployees() {
 		return employeeRepository.findAll();
 	}
+	
+	@DeleteMapping("/employees/{employeeId}")
+	public Employee deleteEmployee(@PathVariable int employeeId) {
+		Optional<Employee> result = employeeRepository.findById(employeeId);
+		Employee employee = result.get();
+		
+		if (employee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		
+		employeeRepository.delete(employee);
+		
+		return employee;
+	}
+	
 
 }
